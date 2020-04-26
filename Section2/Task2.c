aa47e09bafae393ba119f46819eff9d8afa44a0f
Ciaran Hammond <ciaranhammond6895@gmail.com>
2020-04-26 16:34:56 +0000

    Edited Makefile, and edited Task2.c

--- Section2/Task2.c
+++ Section2/Task2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
 
 int main() {
-	int num;
+	system("egrep '^#([a-fA-F0-9]{6}|[a-fA-F0-9]{8})' colours.txt | cut -c 4,5");
+	
+	/*int num;
 	scanf("%x", &num);
-	printf("%d", num);
+	printf("%d", num);*/
 	return 0;
 }
